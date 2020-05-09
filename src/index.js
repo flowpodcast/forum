636841bf70a443d9434b2d2fa8d0c998597ba1ac
Juan Belieni <juanbelieni@gmail.com>
2020-05-09 14:33:17 -0300

    Muda o nome do componente principal e testa o ESLint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MainController from './controller'
+import React from 'react';
 
-ReactDOM.render(<MainController />, document.getElementById('root'));
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
