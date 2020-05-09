53de8fb6c8f24aaffadc51a3431a6ede5139c64d
Leonardo de Andrade <leonardo.andrade@actionlabs.com.br>
2020-05-09 19:41:04 -0300

    Testando deploy automatico

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -98,7 +98,7 @@ export default function Landing() {
                     <div className="personal-infos">
                         <span >GianlucaJux</span>
                         <div className="info-level">
-                            <AiFillStar className="level-ico"></AiFillStar> <span>1239 KD</span>
+                            <AiFillStar className="level-ico"></AiFillStar> <span>1932 KD</span>
                         </div>
                     </div>
                     <details onClick={(e) => handlePersonalMenu(e)}>
