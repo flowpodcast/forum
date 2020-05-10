4dca9777ec4066406a066acad214a9af6fff86d0
Leonardo de Andrade <leonardo.andrade@actionlabs.com.br>
2020-05-09 22:14:30 -0300

    Update index.js

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -98,7 +98,7 @@ export default function Landing() {
                     <div className="personal-infos">
                         <span >GianlucaJux</span>
                         <div className="info-level">
-                            <AiFillStar className="level-ico"></AiFillStar> <span>4000 KD</span>
+                            <AiFillStar className="level-ico"></AiFillStar> <span>4500 KD</span>
                         </div>
                     </div>
                     <details onClick={(e) => handlePersonalMenu(e)}>
